cecc71d25f563b3059efc5178043cdee3b134d26
MrMilanio030 <milanhartland@gmail.com>
2025-08-02 14:41:46 +0200

    Added game to Github

---
+++ Assets/GunHandler.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using MilanUtils;
+using System.Linq;
+using System.Collections.Generic;
+
+public class GunHandler : MonoBehaviour
+{
+    [Tooltip("If raycasts should be used for hit detection or physical bullets")]public bool useRaycast;
+    [Tooltip("The prefab of the bullet"), ShowIf("!useRaycast")]public GameObject bulletPrefab;
+    [Tooltip("The velocity of the bullet"), ShowIf("!useRaycast")]public float shootVelocity;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Transform obj = Instantiate(bulletPrefab).transform;
+            obj.position = transform.position;
+            obj.GetComponent<Rigidbody2D>().linearVelocity = Angle2D.GetAngleFromPos<Vector3, Vector2>(transform.position, UI.WorldMousePos()) * shootVelocity;
+        }
+    }
+}
---
+++ Assets/MovementModifiers.cs
@@ -0,0 +1,83 @@
+using System.Collections.Generic;
+using MilanUtils;
+using static MilanUtils.RepeatedAction;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMovement))]
+public class MovementModifiers : MonoBehaviour
+{
+    PlayerMovement pm;
+    Rigidbody2D rb;
+    Dictionary<string, dynamic> abilityVars = new();
+
+    GameObject canvas;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        pm = GetComponent<PlayerMovement>();
+        rb = GetComponent<Rigidbody2D>();
+        DragDrop.dragInAction += DragInAction;
+        DragDrop.dragOutAction += DragOutAction;
+        canvas = GameObject.Find("Canvas");
+
+        abilityVars.Add("Dash", false); pm.onLand += () => { abilityVars["Dash"] = true; };
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R)) canvas.SetActive(!canvas.activeSelf);
+
+        foreach (DragDrop dd in DragDrop.slottedItems)
+        {
+            if (dd.name != "Movement") continue;
+
+            InfoTag tag = dd.GetComponent<InfoTag>();
+            string name = tag.name;
+            switch (name)
+            {
+                case "Dash":
+                    if (abilityVars[name] && Input.GetKeyDown(KeyCode.LeftShift) && Input.GetAxisRaw("Horizontal") != 0f)
+                    {
+                        float xBefore = rb.linearVelocityX;
+
+                        rb.linearVelocityY = Input.GetAxisRaw("Vertical") * tag.value;
+                        if (Mathf.Abs(rb.linearVelocityX) >= pm.maxMoveSpeed + tag.value) rb.linearVelocityX += Input.GetAxisRaw("Horizontal") * tag.value;
+                        else rb.linearVelocityX = Input.GetAxisRaw("Horizontal") * (pm.maxMoveSpeed + tag.value);
+
+                        Vector2 vel = rb.linearVelocity;
+                        
+                        RepeatActionBoth(0.5f,
+                        () => {rb.linearVelocity = vel; return pm.grounded || pm.touchingWall; },
+                        () => { rb.linearVelocityX = xBefore; });
+
+                        abilityVars[name] = false;
+                    }
+                    break;
+
+                default: Debug.LogError($"No valid Movement found ({tag.name})"); break;
+            }
+        }
+    }
+
+    void DragInAction(DragDrop dd) { ApplyChanges(dd, true); }
+    void DragOutAction(DragDrop dd) { ApplyChanges(dd, false); }
+
+    void ApplyChanges(DragDrop dd, bool add)
+    {
+        if (dd.name.Contains("Modifier")) ApplyModifiers(dd.GetComponent<InfoTag>(), add);
+        else if (dd.name.Contains("Movement")) return;
+        else Debug.LogError($"No valid DragDrop name found ({dd.name})");
+    }
+
+    void ApplyModifiers(InfoTag tag, bool add)
+    {
+        switch (tag.name)
+        {
+            case "Jump Height":
+                pm.jumpVel += add ? tag.value : -tag.value;
+                break;
+            default: Debug.LogError($"No valid Modifier found ({tag.name})"); break;
+        }
+    }
+}
