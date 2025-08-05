4698820f4118944ad2e337ce7a9da6a126486f46
MrMilanio030 <milanhartland@gmail.com>
2025-08-05 11:12:23 +0200

    Moved Pathfinding.cs and Dijkstra to statics

--- Assets/BetterEnemyAI.cs
+++ Assets/BetterEnemyAI.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using MilanUtils;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class BetterEnemyAI : MonoBehaviour
 {
@@ -11,21 +12,22 @@ public class BetterEnemyAI : MonoBehaviour
 
     Rigidbody2D rb;
 
-    public Dictionary<Vector3, List<Vector3>> pathGraph = new();
+    public static Dictionary<Vector3, List<Vector3>> pathGraph = new();
     List<Vector3> path = new();
+
+    public Tilemap map;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        if (pathGraph.Count == 0) pathGraph = Pathfinding.GenerateDijkstraGraph(map);
         rb = GetComponent<Rigidbody2D>();
-
-        GenerateMap();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Z)) GetPath(transform.position, World.mousePos);
+        if (Input.GetKeyDown(KeyCode.Z)) path = Pathfinding.Dijkstra(transform.position, World.mousePos, pathGraph);
     }
 
     void Pathfind(Vector3 target) { StartCoroutine(PathfindCoroutine(target)); }
@@ -64,82 +66,6 @@ public class BetterEnemyAI : MonoBehaviour
         return closest;
     }
 
-    void GenerateMap()
-    {
-        List<Vector3> allNodes = new();
-        foreach (Transform child in GameObject.Find("Path Node Parent").transform)
-        { child.position = Vector3Int.RoundToInt(child.position); allNodes.Add(child.position); }
-
-        pathGraph = new();
-        foreach (Vector3 a in allNodes)
-        {
-            pathGraph.Add(a, new());
-            foreach (Vector3 b in allNodes)
-            {
-                if (a == b) continue;
-                RaycastHit2D hit = Physics2D.Linecast(a, b, ~(1 << gameObject.layer));
-                if (!hit) { pathGraph[a].Add(b); }
-            }
-        }
-    }
-
-    class PathNode
-    {
-        public Vector3 pos;
-        public float dist;
-        public PathNode host;
-    }
-
-    void GetPath(Vector3 start, Vector3 end)
-    {
-        GenerateMap();
-
-        float lowestEndDist = Mathf.Infinity; float lowestStartDist = Mathf.Infinity; Vector3 newEnd = end; Vector3 newStart = start;
-        foreach (Vector3 node in pathGraph.Keys)
-        {
-            float startDist = Vector2.Distance(node, start); if (startDist < lowestStartDist) { lowestStartDist = startDist; newStart = node; }
-            float endDist = Vector2.Distance(node, end); if (endDist < lowestEndDist) { lowestEndDist = endDist; newEnd = node; }
-        }
-        start = newStart; end = newEnd;
-
-        Dictionary<Vector3, PathNode> pathNodes = new();
-        List<PathNode> unvisited = new() { new() { pos = start } };
-
-        foreach (Vector3 v in pathGraph.Keys) { pathNodes.Add(v, new() { pos = v, dist = Mathf.Infinity, host = null }); }
-
-        PathNode cur = null;
-        for (int i = 0; i < 1000; i++)
-        {
-            unvisited.Sort((x, y) => x.dist.CompareTo(y.dist));
-            if (unvisited[0].dist == Mathf.Infinity) { path = new(); return; }
-            cur = unvisited[0];
-            unvisited.Remove(cur);
-
-            print($"{cur.pos} {end}");
-            if (cur.pos == end) { path = new(); RetracePath(cur); break; }
-
-            foreach (Vector3 connection in pathGraph[cur.pos])
-            {
-                PathNode con = pathNodes[connection];
-                float newDist = cur.dist + Vector2.Distance(cur.pos, con.pos);
-                PathNode newNode = new() { pos = connection, dist = newDist, host = cur };
-
-                try { unvisited.First(x => x.pos == connection); } catch { unvisited.Add(newNode);}
-
-                if (con.dist > newDist) con = newNode;
-            }
-        }
-
-        path.Reverse();
-        return;
-
-        void RetracePath(PathNode cur)
-        {
-            path.Add(cur.pos);
-            if (cur.host != null) RetracePath(cur.host);
-        }
-    }
-
     void OnDrawGizmos()
     {
         // foreach (var obj in pathGraph)
@@ -150,11 +76,13 @@ public class BetterEnemyAI : MonoBehaviour
         //     }
         // }
 
-        foreach(var obj in pathGraph){ Gizmos.DrawCube(obj.Key, Vector3.one * 0.5f); }
-        
-        // for (int i = 0; i < path.Count; i++)
-        // {
-        //     if (i < path.Count - 1) Gizmos.DrawLine(path[i], path[i + 1]);
-        // }
+        foreach (var obj in pathGraph.Keys) { Gizmos.DrawCube(obj, Vector3.one * 0.5f); }
+
+        if (path.Count < 2) return;
+        for (int i = 0; i < path.Count; i++)
+        {
+            if (i < path.Count - 1) Gizmos.DrawLine(path[i], path[i + 1]);
+        }
+        Gizmos.DrawLine(transform.position, path[0]); Gizmos.DrawLine(World.mousePos, path[^1]);
     }
 }
