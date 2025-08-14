48ff9f56ac16f309b5dde38b2f66e7da95caf9d6
MrMilanio030 <milanhartland@gmail.com>
2025-08-14 21:28:45 +0200

    Improved Enemy Pathfinding

--- Assets/BetterEnemyAI.cs
+++ Assets/BetterEnemyAI.cs
@@ -14,42 +14,58 @@ public class BetterEnemyAI : MonoBehaviour
 
     public static Dictionary<Vector3, List<Vector3>> pathGraph = new();
     List<Vector3> path = new();
+    Vector3 curTarget = Vector3.zero;
 
     public Tilemap map;
+    public Bounds floorBounds;
+    bool grounded => Physics2D.OverlapBox(transform.position + floorBounds.center, floorBounds.size, 0f, ~(1 << gameObject.layer));
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        if (pathGraph.Count == 0) pathGraph = Pathfinding.GenerateDijkstraGraph(map);
         rb = GetComponent<Rigidbody2D>();
+
+        Pathfinding.ConnectionRequirements requirements = (a, b) =>
+        {
+            Vector2 diff = b - a;
+            bool corrX = Mathf.Abs(diff.x) <= 2f*speed - Mathf.Clamp(diff.y, 0f, maxJumpHeight);
+            bool corrY = diff.y <= maxJumpHeight;
+
+            return corrX && corrY;
+        };
+        pathGraph = Pathfinding.GenerateMapDijkstraGraphFull(map, true, requirements, gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Z)) path = Pathfinding.Dijkstra(transform.position, World.mousePos, pathGraph);
+        if (Input.GetKeyDown(KeyCode.Z)) Pathfind(World.mousePos);
     }
 
     void Pathfind(Vector3 target) { StartCoroutine(PathfindCoroutine(target)); }
     IEnumerator PathfindCoroutine(Vector3 target)
     {
-        Vector3 curTarget = target;
+        path = Pathfinding.Dijkstra(transform.position, target, pathGraph);
+
+        if(path.Count < 2){ print("NO PATH"); yield break; }
+        curTarget = path[0]; path.RemoveAt(0);
         while (Vector2.Distance(transform.position, target) > 0.05f)
         {
-            RaycastHit2D lineObstructed = Physics2D.Linecast(transform.position, target, ~(1 << gameObject.layer));
-            if (lineObstructed)
+            if (Vector2.Distance(curTarget, transform.position) <= 0.1f)
             {
-
+                if (path.Count > 0) { curTarget = path[0]; path.RemoveAt(0); }
+                else { rb.linearVelocityX = 0f; yield break; }
             }
+
+            if (curTarget.y - transform.position.y > 0.05f && grounded) JumpTo(curTarget);
+            rb.linearVelocityX = Mathf.Sign(curTarget.x - transform.position.x) * speed;
+
             yield return new WaitForFixedUpdate();
         }
     }
 
     public void JumpTo(Vector3 target)
     {
-        //MAKE THE ENEMY CALCULATE THE VELOCITY OR HEIGHT FOR THE JUMP WHEN MOVING AT CONSTANT SPEED
-        //ALTERNATIVELY CHEAT IT SOMEHOW (then the problem'd be how to cheat lol)
-
         Vector2 diff = target - transform.position;
 
         float g = -Physics2D.gravity.y;
@@ -59,30 +75,24 @@ public class BetterEnemyAI : MonoBehaviour
         rb.linearVelocity = vel;
     }
 
-    Vector3 GetClosestNode()
-    {
-        float min = Mathf.Infinity; Vector3 closest = Vector3.positiveInfinity;
-        foreach (Vector3 vec in pathGraph.Keys) { if (Vector2.Distance(transform.position, vec) <= min) { closest = vec; } }
-        return closest;
-    }
-
     void OnDrawGizmos()
     {
-        // foreach (var obj in pathGraph)
-        // {
-        //     foreach (var listObj in obj.Value)
-        //     {
-        //         Gizmos.DrawLine(obj.Key, listObj);
-        //     }
-        // }
-
-        foreach (var obj in pathGraph.Keys) { Gizmos.DrawCube(obj, Vector3.one * 0.5f); }
-
-        if (path.Count < 2) return;
-        for (int i = 0; i < path.Count; i++)
+        foreach (var obj in pathGraph.Keys)
         {
-            if (i < path.Count - 1) Gizmos.DrawLine(path[i], path[i + 1]);
+            Gizmos.DrawCube(obj, Vector3.one * 0.5f);
+            foreach (var b in pathGraph[obj]) Gizmos.DrawLine(obj, b);
         }
-        Gizmos.DrawLine(transform.position, path[0]); Gizmos.DrawLine(World.mousePos, path[^1]);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(curTarget, Vector3.one);
+
+        Gizmos.color = Color.blue;
+        if (path.Count < 1) return;
+        Gizmos.DrawLine(transform.position, curTarget); Gizmos.DrawLine(curTarget, path[0]);
+        if (path.Count < 2) return;
+        for (int i = 0; i < path.Count - 1; i++)
+        {
+            Gizmos.DrawLine(path[i], path[i + 1]);
+        }
     }
 }
